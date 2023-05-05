f804d9f4ff0655b09b2504ef5abe41ff171fbd93
aurora-0025 <ritingeorge.d@gmail.com>
2023-05-05 10:07:37 +0530

    test commit

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -13,6 +13,8 @@ const config = {
     appId: process.env.NEXT_PUBLIC_FIREBASE_APPID,
 };
 
+console.log(config);
+
 const app = initializeApp(config);
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
